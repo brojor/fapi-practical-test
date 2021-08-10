2a11fa6a36b4f9e7a722bd50fc69e9f4b85a90bd
brojor <bronislav.jordan@gmail.com>
2021-08-10 21:56:48 +0200

    styling recapitulation page

--- src/views/Recapitulation.vue
+++ src/views/Recapitulation.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="about">
-    <h1>Rekapitulace</h1>
-    <select name="" id="" v-model="currency">
-      <option v-for="currency in rates" :key="currency.code" :value="currency">{{
-        `${currency.code} - (${currency.country})`
-      }}</option>
-    </select>
-    <table>
-      <tr>
-        <th>Produkt</th>
-        <th>Cena / ks (bez DPH)</th>
-        <th>Počet kusů</th>
-        <th>Cena celkem (bez DPH)</th>
-        <th>Cena (s DPH)</th>
-      </tr>
-      <tr v-for="item in cart" :key="item.id">
-        <td>{{ item.name }}</td>
-        <td>{{ priceWithoutVAT(item).toFixed(2) }} {{currency.code}}</td>
-        <td>{{ item.amount }}</td>
-        <td>{{ totalPriceWithoutVAT(item).toFixed(2) }} {{currency.code}}</td>
-        <td>{{ totalPriceWithVAT(item).toFixed(2) }} {{currency.code}}</td>
-      </tr>
-    </table>
+  <div class="recap">
+    <div class="container">
+      <h1>Rekapitulace</h1>
+      <select name="" id="" v-model="currency">
+        <option v-for="currency in rates" :key="currency.code" :value="currency">{{
+          `${currency.code} - (${currency.country})`
+        }}</option>
+      </select>
+      <table>
+        <tr>
+          <th>Produkt</th>
+          <th>Cena / ks (bez&#160;DPH)</th>
+          <th>Počet kusů</th>
+          <th>Cena celkem (bez&#160;DPH)</th>
+          <th>Cena (s&#160;DPH)</th>
+        </tr>
+        <tr v-for="item in cart" :key="item.id">
+          <td>{{ item.name }}</td>
+          <td>{{ priceWithoutVAT(item).toFixed(2) }} {{ currency.code }}</td>
+          <td>{{ item.amount }}</td>
+          <td>{{ totalPriceWithoutVAT(item).toFixed(2) }} {{ currency.code }}</td>
+          <td>{{ totalPriceWithVAT(item).toFixed(2) }} {{ currency.code }}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 <script>
@@ -75,16 +77,66 @@ export default {
 </script>
 
 <style scoped>
-.about {
-  flex-grow: 1;
-}
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-  padding: 0.5rem;
 
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap');
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 65%;
+  width: 95%;
+  padding: 2rem;
+  max-width: 700px;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 1px 4px;
+}
+
+.recap {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+h1 {
+  margin-bottom: 2rem;
+}
+select {
+  margin-bottom: 1rem;
+  width: 95%;
+  padding: 0.5rem 1rem;
+  margin: 1rem;
+  border: 1px solid rgb(65, 65, 65);
+  border-radius: 4px;
+}
+table {
+  width: 95%;
+  border-radius: 4px;
+  border-style: hidden; /* hide standard table (collapsed) border */
+  box-shadow: 0 0 0 1px rgb(0, 0, 0); /* this draws the table border  */
+  border-collapse: collapse;
+  overflow: hidden;
+}
+
+th,
+td {
+  border: 1px solid black;
+  padding: 0.5rem;
+}
+th {
+  background-color: #acd2fc;
 }
 td {
   text-align: right;
+  font-family: 'Roboto Mono', monospace;
+  white-space: nowrap;
+}
+td:first-child {
+  text-align-last: left;
+}
+tr:nth-child(2n + 3) {
+  background-color: #e3f0ff;
 }
 </style>
